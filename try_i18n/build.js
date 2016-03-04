2afcf6027780b765ccf8fb8733c25fc45fe04079
Martin Heidegger <martin.heidegger@gmail.com>
2016-03-04 23:04:41 +0900

    Step "残りのコンテンツを翻訳"

--- try_i18n/build.js
+++ try_i18n/build.js
@@ -21,13 +21,29 @@ processFiles(SOURCE, path.join(TARGET, 'ja'), {
   title: 'クール ホームページ',
   lang: 'ja',
   otherLang: 'en',
-  otherLangName: 'English'
+  otherLangName: 'English',
+  index: {
+    title: 'Ｆｒｏｎｔｅｎｄ  Ｃｏｎｆｅｒｅｎｃｅ  ２０１６にようこそ!',
+    content: '今日は国際化を学びましょう。プロダクトやエンタテインメントを世界中でシェアできるように。私たちのスキルは関西のために大切です。'
+  },
+  product: {
+    title: '会議用プロダクト!',
+    content: '何かを売らないといけません。この仕事でお金が入った方がいいですよね？'
+  }
 })
 processFiles(SOURCE, path.join(TARGET, 'en'), {
   title: 'Cool Homepage',
   lang: 'en',
   otherLang: 'ja',
-  otherLangName: '日本語'
+  otherLangName: '日本語',
+  index: {
+    title: 'Welcome to the Frontend Conference 2016!',
+    content: 'Today we will study internationalization to bring commerce and entertainment from and to Osaka. Our skill is important for making Kansai better.'
+  },
+  product: {
+    title: 'Product for the conference!',
+    content: 'Because you will need to sell something. It isn\'t gonna pay for itself.'
+  }
 })
 
 function processFiles (source, target, variables) {
